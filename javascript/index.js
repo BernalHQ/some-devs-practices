56bcab4c97cba8081f173b291cc8cac2ce26cb19
BernalHQ <bernalhq19@gmail.com>
2024-04-10 23:15:49 -0600

    code

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,21 @@
-console.log("Hello world!!")
+const side = 30
+const half = 15
+let row = ""
+let startPaint = 15;
+
+for (let i = 1; i <= side; i++) {
+    for (let j = 1; j <= side; j++) {
+        if (j === half || j === startPaint) {
+            row += "*"
+            startPaint = half - 1
+            continue
+        } else if (j > startPaint && j <= half) {
+            row += "*"
+            startPaint = half - 1
+            continue
+        }
+        row += " "
+    }
+    console.log(row)
+    row = "";
+}
