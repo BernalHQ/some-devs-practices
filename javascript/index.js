97bfe8fa527bbbf233c340864aafeb861416d3a1
BernalHQ <bernalhq19@gmail.com>
2024-04-11 23:58:30 -0600

    rhomboid

--- javascript/index.js
+++ javascript/index.js
@@ -47,35 +47,32 @@ tree()
 
 
 const rhomboid = () => {
-    for (let i = 1; i <= 25; i++) {
-        for (let j = 0; j <= side; j++) {
-            if (j === 0 && startPaint === 0) {
-                row += " "
-                buildBaseTree = true
-                startPaint++
-                continue
-            } else if (j === startPaint) {
-                row += "*"
-                
-                if (buildBaseTree === true){
-                    
-                    startPaint++
-                    endStart--
-                } else {
-                    startPaint--
-                    endStart++
-                }
-                continue
-            } else if (j > startPaint && j <= endStart) {
+    let block = false
+
+    for (let i = 1; i <= side; i++) {
+        for (let j = 1; j <= side; j++) {
+    
+
+            if (j >= startPaint && j <= endStart) {
                 row += "*"
                 continue
             } 
+
             row += " "
+        }
+        if (i >= 15) {
+            startPaint++
+            endStart--
+        } else {
+            startPaint--
+            endStart++
         }
         console.log(row)
         row = ""
     }
     clearVariables()
 }
+
+
 
 rhomboid()
